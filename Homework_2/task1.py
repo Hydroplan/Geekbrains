2bdefda2f5df9418b14886f0e450626d7ac226cd
Efimov Nikita <studentspbgpu@mai.ru>
2022-04-12 15:53:39 +0300

    all tasks №2 done

--- Homework_2/task1.py
+++ Homework_2/task1.py
@@ -2,7 +2,4 @@ print(type(15 * 3))
 print(type(15 / 3))
 print(type(15 // 2))
 print(type(15 ** 2))
-new_str = '12 hhh 123'
-new_str = new_str.replace(' ','',1)
-print(new_str)
-# print(help(str))
+
